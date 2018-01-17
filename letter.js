26d5746b190f8b90144a4902f598b714d98c51fe
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-17 15:44:59 -0500

    changes to letter.js, word.js

--- letter.js
+++ letter.js
@@ -1,4 +1,5 @@
-// Letter.js file is used to render the letter. (Determine how to display letters for the user.) Either as the letter or a "_" blank.
+// Letter.js file is used to determine how to display the input from the user. Either as the letter or a "_" blank.
+// Do I need to access the random word in this file?????
 
 // Just to test...
 // var letter = process.argv[2];
@@ -8,16 +9,28 @@
 var Letter = function(letter) {
     // Set the variable for what the user enters.
     this.letter = letter;
-    console.log("Letter: ", this.letter);
+    this.blank = "_";
+    console.log("From letter.js...Letter: ", this.letter);
     // Method that prints the letter info:
-    this.printInfo = function() {
-        console.log("And, letter: ", this.letter);
+    this.getLetter = function() {
+        console.log("From letter.js...And, getLetter: ", this.letter);
       };
-}
+    this.displayLetter = function() {
+        // Need a method here that determines how to display the letter, either as letter or "_". 
+        // **Q** How do I write this if I can't "see" the random word??? 
+        if (this.letter = letter) {
+            this.displayLetter = true;
+        } else {
+            return this.blank;
+        }
+        console.log("display correct character: ", this.displayLetter);
+        }
+    }
+
 // Sets a new variable for a new letter input:
 var input = new Letter (process.argv[2]);
-// calls the printInfo method:
-input.printInfo();
+// calls the getLetter method:
+input.getLetter();
 
 // Exports the Letter constructor. It will be required in the word.js file.
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,81 +1,148 @@
-// Word.js file is used to check the random word from the word bank against the letters guessed.
+// Word.js file is used to:
+// (method checkLetters) Check the random word from the word bank against the letters guessed.
+// 
 
 // Require the Letter constructor function that was exported from letter.js.
 var Letter = require("./letter");
 
 // Global variables (Pulled from original hw assignment):
-    // Variable that represents the word bank.
-    var wordBank = ["acadia", "everglades", "yosemite", "yellowstone", "glacier", "badlands", "shenandoah", "arches", "zion", "canaveral", "denali", "gettysburg", "antietam"];
-    console.log("Words: ", wordBank);
-    // variable to break the solution up into indv letter to be stored in the array:
-    var lettersInChosenWord = [];
-    // number of blanks shown based on solution.
-    var numBlanks = 0;
-    // mixture of blanks and correct letters.
-    var blanksAndSuccesses = [];
-    // holds all the wrong guesses.
-    var guessWrongArray = [];
-    // Variable that represents the solution.
-    var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+// Just to test...
+var letter = process.argv[2];
+
+// Variable that represents the word bank. **Q** Does this go in word.js or letter.js?
+var wordBank = ["acadia", "everglades", "yosemite", "yellowstone", "glacier", "badlands", "shenandoah", "arches", "zion", "canaveral", "denali", "gettysburg", "antietam"];
+console.log("From word.js...Words: ", wordBank);
+// variable to break the solution up into indv letter to be stored in the array:
+var lettersInChosenWord = [];
+// number of blanks shown based on solution.
+var numBlanks = 0;
+// mixture of blanks and correct letters.
+var blanksAndSuccesses = [];
+// holds all the wrong guesses.
+var guessWrongArray = [];
+// Variable that represents the solution.
+var randomWord = "";
+console.log("Random Word-Blank: ", randomWord);
+// Variable at beginning of the game.
+var guessesLeft = 10;
+console.log("Guesses Left: ", guessesLeft);
+
+// TEST THIS in isolation for randomWord:
+// This is the startGame function from original HW. Needs to be made into a method within the new Word constructor function or Letter constructor function. **Q** Does this go in word.js or letter.js? 
+function startGame() {
+    // choose solution randomly from the parksList.
+    randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log("From word.js...Random Word: ", randomWord);
+    // break the word up into indv letters. Then count the letters in the word.
+    lettersInChosenWord = randomWord.split("");
+    console.log("Letters in Chosen Word (split): ", lettersInChosenWord);
+    numBlanks = lettersInChosenWord.length;
     console.log("Random Word: ", randomWord);
-    // Variable at beginning of the game.
-    var guessesLeft = 10;
+    // Now, reset the guess/success array after each round.
+    blanksAndSuccesses = [];
+    // And, reset the worng guesses after each round.
+    guessWrongArray = [];
+    // Fill up the blanks and successes list with the correct number of blanks based on the number of letters in the solution.
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
+    console.log("# of Blanks: ", blanksAndSuccesses);
+
+    // Puts the guesses left back to 10
+    console.log("Guesses Left: ", guessesLeft);
+    // Prints the blanks at the beginning of each round.
+    console.log("Join blanks and successes: ", blanksAndSuccesses.join(" "));
+    // clears the wrong guesses from the previous round.
+    console.log("Clear the guesses: ", guessWrongArray.join(" "));
+}
+// call the function - this WORKS in isolation.
+startGame();
+
+// TEST THIS in isolation for the checkLetters function from the original HW assignment:
+// **Q** Does this go in letter.js or word.js????
+function checkLetters(letter) {
+    // create boolean toggle based on whether a user letter is found in the word.
+    var letterInWord = false;
+    for (var i = 0; i < numBlanks; i++) {
+        // ==>
+        if (randomWord[i] === letter) {
+            letterInWord = true;
+        }
+    }
+    // if the letter exists in the word, then find out where it belongs.
+    if (letterInWord) {
+        // Loop through the word
+        for (var j = 0; j < numBlanks; j++) {
+            if (randomWord[j] === letter) {
+                // put it in the correct spot.
+                blanksAndSuccesses[j] = letter;
+            }
+        }
+        console.log("Correct Spot?: ", blanksAndSuccesses);
+    } else {
+        // add the letter to the list of wrong letters and subtract one of the guesses.
+        guessWrongArray.push(letter);
+        guessesLeft--;
+    }
+}
+checkLetters(letter);
+// call the function - this WORKS in isolation - if the chosen letter is correct, it does place it in the right spot.
+
+// TEST THIS in isolation...this is the roundComplete function that runs after each guess is made.
+function roundComplete() {
+
+    // First, log an initial status update guesses are left.
     console.log("Guesses Left: ", guessesLeft);
 
+    // update the the cli to reflect the new number of guesses and update the correct guesses.
+    console.log("word-blanks: ", blanksAndSuccesses.join(" "));
+    console.log("letters-used: ", guessWrongArray.join(" "));
+
+    // if all the letters match the solution.
+    if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+        console.log("You won!")
+        startGame();
+    } else if (guessesLeft === 0) {
+        console.log("You lost!")
+        startGame();
+    }
+}
+roundComplete();
+
+// Create the main function to run the game:
+// function runGame() {
+//     checkLetters(letter);
+//     roundComplete();
+//     startGame();
+// }
+// runGame();
+
+// ------------------------------------------------------------------------------------
 // CONSTRUCTOR FUNCTION for creating the Word objects:
-var Word = function(randomWord) {
+var Word = function (randomWord) {
     // To display a random word:
     this.randomWord = randomWord;
-    console.log("And, Random Word: ", this.randomWord);
-    // this.letter will hold all the Letter objects
-    this.letter = [];
-    console.log("Letter: ", this.letter);
-     // Method that prints the letter info:
-     this.printInfo = function() {
+    console.log("From word.js...And, Random Word: ", this.randomWord);
+
+    // this.letterArray will hold all the Letter objects!!
+    this.letterArray = [];
+    console.log("From word.js...Letter: ", this.letterArray);
+    
+    // True or false, was the letter found?
+
+    // Method to construct letter objects from the random word and then prints the letter info? (or pushes to letterArray):
+    this.getLetter = function () {
         console.log("And, word: ", this.randomWord);
         console.log("And, letter: ", this.letter);
-      };
+    };
+    
     // ...add other parameters for the Word object.
     // this., etc.
-    
-    // ...Do I add methods here to pull in information from the letter.js file? Such as:
-    // method for filling up the blanks and successes list with the correct number of blanks based on the number of letters in the solution. (From the StartGame() in the original hw.)
-    // method for completing all of the comparisons to match letters to guesses, etc. (Part III function from original hw.)
-    // Method based on whether a user letter is found in the word. (From Part III of original HW)
-//     this.letterInWord = function checkLetters(letter) {
-//         var letterInWord = false;
-//         for (var i = 0; i < numBlanks; i++) {
-//             if (randomWord[i] === letter) {
-//                 letterInWord = true;
-//             }
-//         }
-//         // if the letter exists in the word, then find out where it belongs.
-//         if (letterInWord) {
-//             // Loop through the word
-//             for (var j = 0; j < numBlanks; j++) {
-//                 if (randomWord[j] === letter) {
-//                     // put it in the correct spot.
-//                     blanksAndSuccesses[j] = letter;
-//                 }
-//             }
-//             console.log("Correct Spot?: ", blanksAndSuccesses);
-//         } else {
-//             // add the letter to the list of wrong letters and subtract one of the guesses.
-//             guessWrongArray.push(letter);
-//             guessesLeft--;
-//         }
-//     }
-//     // Add Method for breaking the word up into indv letters. 
-//     this.lettersInChosenWord = randomWord.split("");
-//     console.log("Letters in Chosen Word (split): ", lettersInChosenWord);
-//     // Then counts the letters in the word.
-//     this.numBlanks = lettersInChosenWord.length;
-//     console.log("Blanks: ", numBlanks);
 
-// // The following methods create a Letter object from the constructor function that we required and pushes it to the "this.letter" array.
-// this.addLetter = function(letter) {
-//     this.letter.push(new Letter (letter));
-// };
+    this.startGame = function() {
+        // add code from original startGame?
+    }
+
 };
 
 
