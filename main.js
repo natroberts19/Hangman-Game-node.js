9b884b6d60a0fbafb064db39a5fec5036e078e0a
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-19 18:21:43 -0500

    added legacy code into two methods

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ myWord.displayAnswer();
 myWord.checkLetter("s");
 myWord.checkLetter("u");
 myWord.checkLetter("m");
+myWord.checkLetter("e");
+myWord.checkLetter("r");
 myWord.checkLetter("p");
 
-
-// Globals from legacy game:
-var guessesLeft = 10;
-console.log("Guesses Left: ", guessesLeft);
 
 // Create Inquirer menu to display the word and prompt the user for guesses.
 // Dependency for inquirer npm
--- word.js
+++ word.js
@@ -35,14 +35,19 @@ var Word = function (randomWord) {
     console.log("From word.js..Blanks: ", this.randomWordArray);
     // Create an array to hold the blanks and successes: (I don't think I need this but keeping for now.)
     this.blanksAndSuccesses = [];
+    // Create a starter variable for number of guesses.
+    this.guessesLeft = 10;
+    // Create an array to store wrong guesses.
+    this.guessWrongArray = [];
     // A method to reset the guess/success array after each round. (I don't think I need this but keeping for now. I may need this in the Inquirer menu.)
     this.reset = function () {
-        var guessWrongArray = [];
+        this.guessWrongArray = [];
         for (var i = 0; i < this.numBlanks; i++) {
             this.blanksAndSuccesses.push("_");
         }
     }
     console.log("From reset method: ", this.blanksAndSuccesses);
+
     // This method creates an array for new Letter objects. Then pushes the random word into an array that includes the new Letter objects. The new array will include the objects in the format of {letter: "", blank: "_", guessed: true/false} (Help from Kristi here...)
     this.createLetterArray = function () {
         var letterObjArray = [];
@@ -61,27 +66,64 @@ var Word = function (randomWord) {
             myDisplayWord += myLetter.getDisplayCharacter();
             console.log("From word.js..Current Status of Word: ", myDisplayWord);
         }
-        }
+    }
 
     // This method displays the answer once all the spaces have been filled. (uses the getCharacter prototype from letter.js).
     this.displayAnswer = function () {
         var myDisplayAnswer = "";
         for (var i = 0; i < this.letterObjArray.length; i++) {
             myDisplayAnswer += myLetter.getCharacter();
-            console.log("From word.js..Answer: ", this.myDisplayAnswer);
+            console.log("From word.js..Answer: ", myDisplayAnswer);
         }
-        }
+    }
 
-    // This method will now fill in spaces based on letters guessed. (uses the getCharacter prototype from letter.js).
+    // This method will now fill in spaces based on letters guessed. (Using legacy code here from my Wk 3 game.)
     this.checkLetter = function (guessedLetter) {
-        for (var i = 0; i < this.letterObjArray.length; i++) {
-            if (myLetter.getCharacter() === guessedLetter) {
-                myLetter.guessed = true;
+        // Create boolean toggle based on whether a user letter is found in the word.
+        var letterInWord = false;
+        for (var i = 0; i < this.numBlanks; i++) {
+            if (this.randomWord[i] === guessedLetter) {
+                letterInWord = true;
             }
         }
-        this.displayWord();
-        console.log("From word.js..Check Letter: ", guessedLetter);
+        // If the letter exists in the word, then find out where it belongs.
+        if (letterInWord) {
+            // Loop through the word...
+            for (var j = 0; j < this.numBlanks; j++) {
+                if (this.randomWord[j] === guessedLetter) {
+                    // ...and, put it in the correct spot.
+                    this.blanksAndSuccesses[j] = guessedLetter;
+                }
+            }
+            console.log("Correct Spot?: ", this.blanksAndSuccesses);
+        } else {
+            // Add the letter to the list of wrong letters and subtract one of the guesses.
+            this.guessWrongArray.push(guessedLetter);
+            console.log("Wrong Guess Array: ", this.guessWrongArray);
+            this.guessesLeft--;
+            console.log("Guesses Left: ", this.guessesLeft);
         }
+        console.log("From word.js..Check Letter: ", guessedLetter);
+    }
+    // This method will hold the code that needs to be run after each guess is made. (Using legacy code here from my Wk 3 game.)
+    this.roundComplete = function() {
+        // First, log an initial status update in the console telling us how many wins, losses, and guesses are left.
+        console.log("Guesses Left: ", this.guessesLeft);
+        // update the page to reflect the new number of guesses and update the correct guesses.
+        console.log("word-blanks: ", this.blanksAndSuccesses.join(" "));
+        console.log("letters-used: ", this.guessWrongArray.join(" "));
+        // Check if all the letters match the solution.
+        if (this.randomWordArray.toString() === this.blanksAndSuccesses.toString()) {
+            console.log("You Won");
+            // startGame();
+        } else if (this.guessesLeft === 0) {
+            console.log("You Lost!");
+            // restart the game.
+            // startGame();
+        }
+    }
+    this.roundComplete();
+    
 
 }
 
